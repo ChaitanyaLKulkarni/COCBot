ad79a5483a2923ecd8d1fba2cd1a68769b0537df
Chaitanya Kulkarni <chaitanykulkarni21@gmail.com>
2021-05-06 12:55:18 +0530

    Added till p3 summary, and plan  for others

--- client/src/components/P4ConsPlayers.js
+++ client/src/components/P4ConsPlayers.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-import { primArr, secArr } from "../colors";
 export default function P4ConsPlayers() {
     // Show most played players
     // Show most played players with respect to modes
